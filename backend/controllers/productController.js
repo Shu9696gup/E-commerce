41528ca82a703d396e56f670abfa8bd591f70183
Shubham <shu9696gup@gmail.com>
2022-08-17 19:10:45 +0530

    Product API Done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel.js")
 
 
-//create Product
+//create Product -- admin
 
 exports.createProduct = async (req,res,next) => {
     const product = await Product.create(req.body);
@@ -11,7 +11,73 @@ exports.createProduct = async (req,res,next) => {
     })
 }
 
+//get product details
 
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message:"Route is working fine"})
+exports.getProduct = async (req,res,next)=>{
+    var product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+//update product -- admin
+
+exports.updateProduct = async (req,res,next) => {
+    console.log(req.params.id);
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    })
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+//get all product -- admin
+
+exports.getAllProducts = async (req,res)=>{
+    const products = await Product.find();
+
+
+    res.status(200).json({
+            Success: true,
+            products
+        })
+}
+
+//delete product --admin
+
+exports.deleteProduct = async (req,res,next)=>{
+    var product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+        success:true,
+        product
+    })
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProduct} = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/new").post(createProduct);
+router.route("/products/:id").put(updateProduct).delete(deleteProduct).get(getProduct);
+
 
 
 module.exports = router;
